4b8e4050ace073edc16b0a491997cddf2403e949
Pierre Guéroult <81022221+pierregueroult@users.noreply.github.com>
2024-09-18 19:39:18 +0200

    feat(i18n): add lang action and demo in landing page

---
+++ src/components/languages.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import React, { FormEvent } from "react";
+import changeLang from "@/actions/lang/change";
+import locales from "@/lang/locales";
+import { useLocale } from "next-intl";
+import { Locale } from "@/types/lang";
+
+export default function Languages() {
+  const currentLocale = useLocale();
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const lang: string = ((e.target as HTMLFormElement).elements.namedItem("locale") as HTMLInputElement).value;
+    if (locales.includes(lang as Locale)) await changeLang(lang as Locale);
+  };
+
+  return (
+    <ul className="mb-32 mt-4 flex flex-row items-center justify-center gap-4">
+      {locales.map((locale) => (
+        <li key={locale}>
+          <form onSubmit={(e) => handleSubmit(e)}>
+            <button
+              className={`uppercase ${currentLocale === locale ? "text-black underline" : "text-gray-500"}`}
+              type="submit"
+              name="locale"
+              value={locale}
+            >
+              {locale}
+            </button>
+          </form>
+        </li>
+      ))}
+    </ul>
+  );
+}
