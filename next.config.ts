93241a08e01c872ca7a3d38d68896a38b12050f6
Pierre Guéroult <pierre.gueroult@univ-rouen.fr>
2025-01-27 10:22:49 +0100

    chore(update): move next config file from mjs to ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
+import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin("./src/lang/request.ts");
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig: NextConfig = {};
 
 export default withNextIntl(nextConfig);
