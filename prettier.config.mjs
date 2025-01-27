16f1465ac7503548c8c3f72ee573e668a9fee11c
Pierre Guéroult <pierre.gueroult@univ-rouen.fr>
2025-01-27 10:00:01 +0100

    chore(format): format new prettier config file

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -3,14 +3,14 @@
  * @type {import('prettier').Config}
  */
 const config = {
-  "plugins": ["@trivago/prettier-plugin-sort-imports", "prettier-plugin-tailwindcss"],
-  "importOrder": ["react", "next", "next/(.*)$", "^@/components/(.*)$", "^@/(.*)$", "^[./]", "<THIRD_PARTY_MODULES>"],
-  "printWidth": 120,
-  "tabWidth": 2,
-  "trailingComma": "all",
-  "bracketSpacing": true,
-  "semi": true,
-  "singleQuote": false
-}
+  plugins: ["@trivago/prettier-plugin-sort-imports", "prettier-plugin-tailwindcss"],
+  importOrder: ["react", "next", "next/(.*)$", "^@/components/(.*)$", "^@/(.*)$", "^[./]", "<THIRD_PARTY_MODULES>"],
+  printWidth: 120,
+  tabWidth: 2,
+  trailingComma: "all",
+  bracketSpacing: true,
+  semi: true,
+  singleQuote: false,
+};
 
 export default config;
