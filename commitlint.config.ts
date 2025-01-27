0a6a5c8087d9fa8294909ea2ff2c7fd4148fcb1f
Pierre Guéroult <pierre.gueroult@univ-rouen.fr>
2025-01-27 10:18:07 +0100

    chore(update): update commitlint package and move config file

---
+++ commitlint.config.ts
@@ -0,0 +1,30 @@
+import type { UserConfig } from "@commitlint/types";
+import { RuleConfigSeverity } from "@commitlint/types";
+
+const config: UserConfig = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "fix",
+        "feat",
+        "docs",
+        "hotfix",
+        "refactor",
+        "revert",
+        "deploy",
+        "build",
+        "WIP",
+        "important",
+        "breaking",
+        "chore",
+      ],
+    ],
+    "type-case": [RuleConfigSeverity.Disabled, "always", "lower-case"],
+    "subject-case": [RuleConfigSeverity.Error, "always", "lower-case"],
+  },
+};
+
+export default config;
