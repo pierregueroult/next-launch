aa2860ac50a163dab17e7775f0d00788891716e3
Pierre Guéroult <pierre.gueroult@univ-rouen.fr>
2025-01-27 09:59:15 +0100

    fix(format): move prettier rc file to mjs config file

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,4 +1,8 @@
-{
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import('prettier').Config}
+ */
+const config = {
   "plugins": ["@trivago/prettier-plugin-sort-imports", "prettier-plugin-tailwindcss"],
   "importOrder": ["react", "next", "next/(.*)$", "^@/components/(.*)$", "^@/(.*)$", "^[./]", "<THIRD_PARTY_MODULES>"],
   "printWidth": 120,
@@ -8,3 +12,5 @@
   "semi": true,
   "singleQuote": false
 }
+
+export default config;
