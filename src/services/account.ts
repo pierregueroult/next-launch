8c638515caf1d671ced48bd665d4f13bdbc39940
Pierre Guéroult <pierre.gueroult@univ-rouen.fr>
2025-01-28 09:31:38 +0100

    refactor(eslint): remove unused eslint directives in account service and next-auth types

--- src/services/account.ts
+++ src/services/account.ts
@@ -3,7 +3,6 @@ import { cache } from "react";
 import prisma from "@/db";
 import { Account } from "@/db/types";
 
-/* eslint-disable import/prefer-default-export */
 async function getAccountByUserId(userId: string): Promise<Account | null> {
   return prisma.account.findFirst({
     where: { userId },
