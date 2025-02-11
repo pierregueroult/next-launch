95f62d715f19acd5a3db261655bae2a84fc0e6e0
Pierre Guéroult <pierre.gueroult@univ-rouen.fr>
2025-02-11 12:57:54 +0100

    chore: add ESLint and Prettier configurations with necessary dependencies

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+
+/** @type {import('eslint').Linter.Config[]} */
+const config = [
+  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+];
+
+export default config;
