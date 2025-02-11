fd59d69722b552276c0675f30d4ee0f87280d8ad
Pierre Guéroult <pierre.gueroult@univ-rouen.fr>
2025-02-11 15:41:01 +0100

    fix: add react import to layout and page components

--- cli/install/starter/base/src/app/layout.tsx
+++ cli/install/starter/base/src/app/layout.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
--- cli/install/starter/base/src/app/page.tsx
+++ cli/install/starter/base/src/app/page.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import Image from "next/image";
 import styles from "./page.module.css";
 
