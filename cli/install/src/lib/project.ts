a927e147fdcd98c26e05b04804aad0868374a5ba
Pierre Guéroult <pierre.gueroult@univ-rouen.fr>
2025-02-14 23:59:30 +0100

    feat: implement project directory creation and template file copying

--- cli/install/src/lib/project.ts
+++ cli/install/src/lib/project.ts
@@ -1,9 +1,28 @@
+import * as fs from "node:fs";
+import * as path from "node:path";
+import { fileURLToPath } from "node:url";
 import { RequiredOptions } from "src/schemas/options.js";
 
 export async function setupProject(options: RequiredOptions): Promise<void> {
   // * Step 1: Create the project directory
+  const projectDir: string = path.resolve(process.cwd(), options.name);
+  const baseTemplateDir: string = path.join(path.dirname(fileURLToPath(import.meta.url)), "../../starter/base");
 
   // * Step 2: Copy the base template files
+  try {
+    await fs.promises.mkdir(projectDir);
+  } catch (error) {
+    console.error("It is possible that the folder already exists");
+    if (globalThis.isVerbose) console.error(error);
+    process.exit(1);
+  }
+  try {
+    await fs.promises.cp(baseTemplateDir, projectDir, { recursive: true });
+  } catch (error) {
+    console.error("Error copying template files");
+    if (globalThis.isVerbose) console.error(error);
+    process.exit(1);
+  }
 
   // * Step 3: Add the dependencies of the base template to the project (without installing them)
 
