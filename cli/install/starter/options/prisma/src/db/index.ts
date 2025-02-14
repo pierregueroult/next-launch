00798d177eb559bfccf680f0d336cd77144b7e7f
Pierre Guéroult <pierre.gueroult@univ-rouen.fr>
2025-02-14 22:16:49 +0100

    feat: add prisma setup with commands and dependencies for database integration

---
+++ cli/install/starter/options/prisma/src/db/index.ts
@@ -0,0 +1,8 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+const globalForPrisma = global as unknown as { prisma: typeof prisma };
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export default prisma;
