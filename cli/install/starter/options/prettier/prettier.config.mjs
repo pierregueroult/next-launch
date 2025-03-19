81b1b856b27d70cdef7559edc62a07f5a8ac8f59
Pierre Guéroult <pierre.gueroult@univ-rouen.fr>
2025-03-19 16:05:46 +0100

    feat: add prettier configuration and commands for code formatting

---
+++ cli/install/starter/options/prettier/prettier.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('prettier').Config} */
+const config = {
+  printWidth: 120,
+  tabWidth: 2,
+  trailingComma: "all",
+  bracketSpacing: true,
+  semi: true,
+  singleQuote: false,
+};
+
+export default config;
